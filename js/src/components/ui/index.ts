17b13666aa26b1708aae91526a7d288cdd9e8daf
WawasCode <khalatyanwatsche@gmail.com>
2025-06-02 08:40:18 +0000

    remove unsued components

--- js/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button'
-export { Input } from './Input'
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card'
-export { Sidebar, SidebarHeader, SidebarContent } from './Sidebar'
