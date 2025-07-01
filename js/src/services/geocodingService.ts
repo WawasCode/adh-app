6b873ecd086253ed8877c1c154ae0b55d560e448
David Glaser <d2ave@gmx.de>
2025-07-01 11:16:11 +0000

    Connected geocoding and mapview with user location

--- js/src/services/geocodingService.ts
+++ js/src/services/geocodingService.ts
@@ -1,3 +1,5 @@
+import { useLocationStore } from "@/store/useLocationStore";
+
 export interface PhotonFeature {
   properties: {
     name: string;
@@ -28,9 +30,18 @@ export const searchLocations = async (
     return [];
   }
 
+  const position = useLocationStore.getState().position;
+
+  // Default location
+  const defaultLat = 52.52;
+  const defaultLon = 13.405;
+
+  const lat = position ? position[0] : defaultLat;
+  const lon = position ? position[1] : defaultLon;
+
   try {
     const response = await fetch(
-      `https://photon.komoot.io/api/?q=${encodeURIComponent(query)}&lat=52.52&lon=13.405&limit=10`, // lat and long should be the users location
+      `https://photon.komoot.io/api/?q=${encodeURIComponent(query)}&lat=${lat}&lon=${lon}&limit=10`,
     );
     const data = await response.json();
     return data.features || [];
